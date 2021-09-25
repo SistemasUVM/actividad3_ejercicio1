8fa0d9235088fc2ae27995043f027b275eacd461
josevega <jdelavega36@gmail.com>
2021-09-25 10:18:09 -0500

    Programa que determina si un numero es par o non

---
+++ main.cc
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int x;
+
+    cout << "Ingresa un numero" << endl;
+
+    cin >> x;
+
+    if (x % 2 == 0)
+    {
+        cout << "El numero es par" << endl;
+    }
+    else{
+        cout << "El numero es impar" << endl;
+    }
+
+    return 0;
+}
